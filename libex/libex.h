376531cd66844482df54f67d78ecb84606bd01f9
Sandro Magi <naasking@gmail.com>
2010-09-16 20:53:44 -0400

    -perform error check with RETHROW prior to entering a new TRY block -FINALLY implementation shared between DEBUG and RELEASE modes -documented a possible RETRY operation -explained the unsatisfactory tradeoffs inherent to THROW -documented every macro -removed old documentation

--- libex/libex.h
+++ libex/libex.h
@@ -4,21 +4,20 @@
  * LICENSE: LGPL
  * Copyright 2010: Sandro Magi
  *
- * Examples:
+ * Example:
  *
- * char *foo;
- * LET (foo = (char*)malloc(123)) {
+ * TRY(char *foo) {
+ *     MAYBE(foo = (char*)malloc(123), errno)
+ * } IN {
  *   ... do something with foo
- * } CATCH (ENullRef) {
+ * } HANDLE CATCH (EOutOfMemory) {
  *   ... handle error for foo
  * } FINALLY {
  *   ... finalize any state that has already been allocated
  * }
  *
- * TRY (returnExceptionType()) { ... }
- * CATCH (EBadDescriptor) { ... }
- * CATCH (EFileExists) { ... }
- * FINALLY { ... }
+ * The thrown exception can be any sort of error code of integer type, including
+ * errno, and Windows HRESULT.
  *
  * NOTES:
  * # The full TRY()-IN-HANDLE-FINALLY structure is *mandatory*.
@@ -26,8 +25,8 @@
  * # You *cannot* use any control-flow operators, ie. goto, break, continue,
  *   return, that will *escape* an exception block. Any control-flow that stays
  *   within the same exception block is fine, with the following exception:
- * # You cannot execute a THROW or RETHROW within a loop. Use a flag to exit
- *   the loop, then throw the desired exception.
+ * # You cannot execute a THROW or RETHROW within a loop or a switch. Use a
+ *   flag to exit the loop, then throw the desired exception.
  */
 
 #ifndef __LIBEX__
@@ -59,26 +58,6 @@
  * # OpenSSL allegedly has a sophisticated error system:
  *   http://landheer-cieslak.com/wordpress/error-handling-in-c/
  *   It uses a compile-time allocated error queue that stores various information.
- * # This approach can support early exits with THROW = break, assuming there's a top-level
- *   exc_type local in the function. THROW sets the local, then executes a break, which exits
- *   the switch early, execs the finalizer, and proceeds to the next bloc, which is a
- *   finalizer or a RETURN(X). This means there can be no code after an exception handler,
- *   because this code will assume successful completion of the block with no errors, which
- *   isn't the case. Each CATCH block must set exc_type=ENoError. Each TRY is wrapped in an
- *   conditional that first checks that exc_type=ENoError. This supports pre-condition checks
- *   which run before any exception, and may throw exceptions, ie.
- *     if (check) THROW(ArgumentInvalid);
- *   This may require a new terminator ENDTRY to close the conditional. Or perhaps can encode
- *   whether we are in an exception block at the time THROW was invoked via a bit in exc_type.
- *   If the bit is not set, FINALLY can simply exec: if (BIT(exc_type)) break; This doesn't
- *   handle the case of two exception blocks one after the other.
- * # Alternate approach: try-in-catch, where TRY-IN is wrapped in a do-while, and THROW simply breaks
- *   out of that loop to the exception handling block. TRY no longer takes an expression form.
- *   TRY Foo x = MakeFoo(); { ...foo...bar... if (x) THROW(ArgumentInvalid); ... }
- *   IN { ... }
- *   CATCH(ArgumentInvalid) { ... }
- *   FINALLY
- *
  */
 
 #include <stdlib.h>
@@ -195,36 +174,73 @@ typedef enum exc_type {
  * if no finalization code is necessary.
  */
 
-/* THROWS(...) declares which exceptions may be thrown, and declares a local to
- * store the current exception. */
+/* THROWS(...) declares which exceptions may be thrown. It's purely for
+ * documentation purposes, and declares a hidden local to store the current
+ * exception, and a function-scope loop used for exception propagation. */
 #define THROWS(...) exc_type THROWS = ENoError; do {
-#define DONE } while(0); return THROWS
 
+/* DONE designates the end of a function block, where the exception is returned */
+#define DONE } while(0); return THROWS == EEarlyReturn ? ENoError : THROWS
+
+/* RETURN throws an EEarlyReturn exception which propagates up to the caller */
 #define RETURN THROW(EEarlyReturn)
+
+/* A RETRY operation restarts the entire block again. It would *not* run the
+ * finalizers, and would in fact re-run the TRY block thus re-allocating
+ * resources. Probably not a good idea. */
+/*#define RETRY continue*/
+
+/* THROW raises an exception to an enclosing CATCH */
+/* FIXME: This is the ugliest part of the package, because I either force every
+ * use of THROW to be wrapped in {}, or I forbid users from terminating with
+ * a semi-colon ; contrary to typical C style. I can't wrap in do-while because
+ * "break" must break out of the *outer* loop.
 #define THROW(E) { THROWS = (exc_type)(E); break; }
+
+/* RETHROW re-raises the current exception in the parent scope */
 #define RETHROW break
 #define EXC_CASE(E) THROWS = ENoError; break; E:
 #define __CUR_EXC__ THROWS
+
+/* CATCH and CATCHANY specify handlers for various error types */
 #define CATCH(e) EXC_CASE(case e)
 #define CATCHANY EXC_CASE(default)
 
-/* utility macros for checking function results */
+/* FINALLY closes the scope of the exception handling block and designates
+ * the start of code that finalizes any allocated resources */
+#define FINALLY break; } } while(0); }
+
+/* MAYBE raises the exception R if E evaluates to NULL */
 #define MAYBE(E, R) if (NULL == (E)) THROW(R);
+
+/* ERROR raises the exception E if E evaluates to something other than ENoError */
+#define ERROR(E) THROWS = (E); if (THROWS != ENoError) RETHROW;
+
+/* ERRORE raises the exception R if E evaluates to non-zero */
+#define ERRORE(E, R) if ((E)) THROW(R)
+
+/* TRY begins the exception handling scope, and accepts a list of declarations D.
+ * IN designates the scope which executes if no errors were raised in the
+ * TRY scope.
+ * HANDLE designates the start of the error handlers themselves. */
 
 #ifdef _DEBUG
 
-#define TRY(D) while(THROWS == ENoError) { THROWS = ENoError; { D; do
+#define TRY(D) { if (THROWS != ENoError) RETHROW; do { { D; do
 #define IN while(0); if (THROWS == ENoError)
 #define HANDLE } switch(THROWS) { case ENoError: case EEarlyReturn: break;
-#define FINALLY break; } break; }
 
 #else
 
-#define TRY(D) for(THROWS = ENoError; THROWS == ENoError;) { D; 
-#define IN switch (THROWS) { case ENoError: 
-#define HANDLE EXC_CASE(case EEarlyReturn)
-#define FINALLY break; } break; }
+/* The only real difference between the DEBUG and RELEASE implementations
+ * is that the latter has one less if-test and one less lexical scope. So
+ * unlike DEBUG mode, all CATCH clauses can see the bindings introduced in
+ * the TRY block. */
 
-#endif /*DEBUG*/
+#define TRY(D) { if (THROWS != ENoError) RETHROW; { D; do 
+#define IN while (0); switch (THROWS) { case ENoError: 
+#define HANDLE break; case EEarlyReturn:
+
+#endif /*_DEBUG*/
 
 #endif /*__LIBEX__*/
